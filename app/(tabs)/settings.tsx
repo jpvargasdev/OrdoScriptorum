fa9fb31520c5a731a9adb00854942aec936d5b33
jpvargasdev <vargasm.jp@gmail.com>
2024-12-02 21:30:00 +0100

    chore: Add reset functionality

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,129 +1,99 @@
-import { StyleSheet, Image, Platform } from "react-native";
+import React from 'react';
+import {
+	View,
+	Text,
+	TouchableOpacity,
+	StyleSheet,
+	Alert,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { Collapsible } from "@/components/Collapsible";
-import { ExternalLink } from "@/components/ExternalLink";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
-import { IconSymbol } from "@/components/ui/IconSymbol";
+const SettingsScreen = ({ onDeleteAllData }: { onDeleteAllData: () => void }) => {
+	const handleDeleteAllData = () => {
+		Alert.alert(
+			'Delete All Data',
+			'Are you sure you want to delete all data? This action cannot be undone.',
+			[
+				{ text: 'Cancel', style: 'cancel' },
+				{ text: 'Delete', style: 'destructive', onPress: onDeleteAllData },
+			]
+		);
+	};
 
-export default function TabTwoScreen() {
 	return (
-		<ParallaxScrollView
-			headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
-			headerImage={
-				<IconSymbol
-					size={310}
-					color="#808080"
-					name="chevron.left.forwardslash.chevron.right"
-					style={styles.headerImage}
-				/>
-			}
-		>
-			<ThemedView style={styles.titleContainer}>
-				<ThemedText type="title">Explore</ThemedText>
-			</ThemedView>
-			<ThemedText>
-				This app includes example code to help you get started.
-			</ThemedText>
-			<Collapsible title="File-based routing">
-				<ThemedText>
-					This app has two screens:{" "}
-					<ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-					and{" "}
-					<ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-				</ThemedText>
-				<ThemedText>
-					The layout file in{" "}
-					<ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{" "}
-					sets up the tab navigator.
-				</ThemedText>
-				<ExternalLink href="https://docs.expo.dev/router/introduction">
-					<ThemedText type="link">Learn more</ThemedText>
-				</ExternalLink>
-			</Collapsible>
-			<Collapsible title="Android, iOS, and web support">
-				<ThemedText>
-					You can open this project on Android, iOS, and the web. To open the
-					web version, press <ThemedText type="defaultSemiBold">w</ThemedText>{" "}
-					in the terminal running this project.
-				</ThemedText>
-			</Collapsible>
-			<Collapsible title="Images">
-				<ThemedText>
-					For static images, you can use the{" "}
-					<ThemedText type="defaultSemiBold">@2x</ThemedText> and{" "}
-					<ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to
-					provide files for different screen densities
-				</ThemedText>
-				<Image
-					source={require("@/assets/images/react-logo.png")}
-					style={{ alignSelf: "center" }}
-				/>
-				<ExternalLink href="https://reactnative.dev/docs/images">
-					<ThemedText type="link">Learn more</ThemedText>
-				</ExternalLink>
-			</Collapsible>
-			<Collapsible title="Custom fonts">
-				<ThemedText>
-					Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText>{" "}
-					to see how to load{" "}
-					<ThemedText style={{ fontFamily: "SpaceMono" }}>
-						custom fonts such as this one.
-					</ThemedText>
-				</ThemedText>
-				<ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-					<ThemedText type="link">Learn more</ThemedText>
-				</ExternalLink>
-			</Collapsible>
-			<Collapsible title="Light and dark mode components">
-				<ThemedText>
-					This template has light and dark mode support. The{" "}
-					<ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook
-					lets you inspect what the user's current color scheme is, and so you
-					can adjust UI colors accordingly.
-				</ThemedText>
-				<ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-					<ThemedText type="link">Learn more</ThemedText>
-				</ExternalLink>
-			</Collapsible>
-			<Collapsible title="Animations">
-				<ThemedText>
-					This template includes an example of an animated component. The{" "}
-					<ThemedText type="defaultSemiBold">
-						components/HelloWave.tsx
-					</ThemedText>{" "}
-					component uses the powerful{" "}
-					<ThemedText type="defaultSemiBold">
-						react-native-reanimated
-					</ThemedText>{" "}
-					library to create a waving hand animation.
-				</ThemedText>
-				{Platform.select({
-					ios: (
-						<ThemedText>
-							The{" "}
-							<ThemedText type="defaultSemiBold">
-								components/ParallaxScrollView.tsx
-							</ThemedText>{" "}
-							component provides a parallax effect for the header image.
-						</ThemedText>
-					),
-				})}
-			</Collapsible>
-		</ParallaxScrollView>
+		<SafeAreaView style={styles.container}>
+			{/* Settings Title */}
+			<Text style={styles.header}>Settings</Text>
+
+			{/* List of Settings Options */}
+			<View style={styles.section}>
+				<TouchableOpacity style={styles.option}>
+					<Text style={styles.optionText}>Currency Settings</Text>
+				</TouchableOpacity>
+
+				<TouchableOpacity style={styles.option}>
+					<Text style={styles.optionText}>Budget Allocation</Text>
+				</TouchableOpacity>
+
+				<TouchableOpacity style={styles.option}>
+					<Text style={styles.optionText}>Notifications</Text>
+				</TouchableOpacity>
+			</View>
+
+			{/* Delete All Data Button */}
+			<TouchableOpacity
+				style={styles.deleteButton}
+				onPress={handleDeleteAllData}
+			>
+				<Text style={styles.deleteButtonText}>Delete All Data</Text>
+			</TouchableOpacity>
+		</SafeAreaView>
 	);
-}
+};
+
+export default SettingsScreen;
 
 const styles = StyleSheet.create({
-	headerImage: {
-		color: "#808080",
-		bottom: -90,
-		left: -35,
-		position: "absolute",
+	container: {
+		flex: 1,
+		backgroundColor: '#F5F5F5',
+		padding: 20,
 	},
-	titleContainer: {
-		flexDirection: "row",
-		gap: 8,
+	header: {
+		fontSize: 24,
+		fontWeight: 'bold',
+		marginBottom: 20,
+		textAlign: 'center',
+		color: '#333',
+	},
+	section: {
+		marginVertical: 20,
+	},
+	option: {
+		backgroundColor: '#fff',
+		padding: 15,
+		marginBottom: 10,
+		borderRadius: 8,
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.1,
+		shadowRadius: 3,
+		elevation: 2,
+	},
+	optionText: {
+		fontSize: 16,
+		color: '#333',
+	},
+	deleteButton: {
+		backgroundColor: '#FF4D4D',
+		padding: 15,
+		borderRadius: 8,
+		alignItems: 'center',
+		marginTop: 20,
+	},
+	deleteButtonText: {
+		color: '#fff',
+		fontSize: 16,
+		fontWeight: 'bold',
 	},
 });
--- app/new-transaction.tsx
+++ app/new-transaction.tsx
@@ -7,266 +7,279 @@ import { useAxios } from "@/hooks/useAxios";
 import { router } from "expo-router";
 import React, { useCallback, useMemo, useState } from "react";
 import {
-	View,
-	Text,
-	TextInput,
-	TouchableOpacity,
-	StyleSheet,
-	KeyboardAvoidingView,
-	Platform,
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet,
+    KeyboardAvoidingView,
+    Platform,
 } from "react-native";
 
 export default function NewTransaction() {
-	const { data: categories } = useAxios<Category[]>(getCategories);
-	const { data: accounts } = useAxios<Account[]>(getAccounts);
+    const { data: categories } = useAxios<Category[]>(getCategories);
+    const { data: accounts } = useAxios<Account[]>(getAccounts);
 
-	const [amount, setAmount] = useState("");
-	const [category, setCategory] = useState("Add a category");
-	const [date, setDate] = useState(new Date());
-	const [description, setDescription] = useState("");
-	const [type, setType] = useState("Expense");
-	const [currency, setCurrency] = useState("SEK");
-	const [account, setAccount] = useState(accounts?.[0].name);
+    const [amount, setAmount] = useState("");
+    const [category, setCategory] = useState("Add a category");
+    const [date, setDate] = useState(new Date());
+    const [description, setDescription] = useState("");
+    const [type, setType] = useState("Expense");
+    const [currency, setCurrency] = useState("SEK");
+    const [account, setAccount] = useState(accounts?.[0].name);
 
-	const backgroundStyle = useMemo(() => {
-		if (type === "Expense") {
-			return "expenseBg";
-		}
-		if (type === "Transfer") {
-			return "transferBg";
-		}
-		if (type === "Income") {
-			return "incomeBg";
-		}
-		if (type === "Savings") {
-			return "savingsBg";
-		}
-	}, [type]);
+    const backgroundStyle = useMemo(() => {
+        if (type === "Expense") {
+            return "expenseBg";
+        }
+        if (type === "Transfer") {
+            return "transferBg";
+        }
+        if (type === "Income") {
+            return "incomeBg";
+        }
+        if (type === "Savings") {
+            return "savingsBg";
+        }
+    }, [type]);
 
-	const handleKeyPress = (key: string) => {
-		if (key === "delete") {
-			setAmount(amount.slice(0, -1));
-		} else {
-			setAmount(amount + key);
-		}
-	};
+    const handleKeyPress = (key: string) => {
+        if (key === "delete") {
+            setAmount(amount.slice(0, -1));
+        } else {
+            setAmount(amount + key);
+        }
+    };
 
-	const onSubmit = useCallback(async () => {
-		const transaction: Omit<
-			Transaction,
-			| "id"
-			| "amount_in_base_currency"
-			| "exchange_rate"
-			| "main_category"
-			| "subcategory"
-		> = {
-			amount: type === "Expense" ? parseFloat(amount) * -1 : parseFloat(amount),
-			date: Math.floor(date.getTime() / 1000),
-			description,
-			currency,
-			category_id: categories?.find((c) => c.name === category)?.id,
-			transaction_type: type,
-			account_id: 1,
-		};
+    const onSubmit = useCallback(async () => {
+        const transaction: Omit<
+            Transaction,
+            | "id"
+            | "amount_in_base_currency"
+            | "exchange_rate"
+            | "main_category"
+            | "subcategory"
+        > = {
+            amount: type === "Expense" ? parseFloat(amount) * -1 : parseFloat(amount),
+            date: Math.floor(date.getTime() / 1000),
+            description,
+            currency,
+            category_id: categories?.find((c) => c.name === category)?.id,
+            transaction_type: type,
+            account_id: 1,
+        };
 
-		try {
-			await addTransaction(transaction);
-		} catch (error) {
-			console.log(error);
-			alert("Error adding transaction");
-			return;
-		} finally {
-			router.back();
-		}
-	}, [amount, category, date, description, type, currency, account]);
+        // validate transaction fields
+        if (
+            !transaction.amount ||
+            !transaction.date ||
+            !transaction.description ||
+            !transaction.currency ||
+            !transaction.category_id ||
+            !transaction.transaction_type
+        ) {
+            alert("All fields are required");
+            return;
+        }
 
-	return (
-		<KeyboardAvoidingView
-			style={{
-				...styles.container,
-				...styles[backgroundStyle as keyof typeof styles],
-			}}
-			behavior={Platform.OS === "ios" ? "padding" : undefined}
-		>
-			{/* Header */}
-			<View style={styles.header}>
-				<ThemedText type="subtitle">Add Transaction</ThemedText>
-			</View>
+        try {
+            await addTransaction(transaction);
+        } catch (error) {
+            console.log(error);
+            alert("Error adding transaction");
+            return;
+        } finally {
+            router.back();
+        }
+    }, [amount, category, date, description, type, currency, account]);
 
-			{/* Amount Section */}
-			<View style={styles.amountContainer}>
-				<Text style={styles.amount}>
-					{type === "Expense" && "-"}
-					{amount.length > 0 ? amount : "0.00"}
-				</Text>
-				<CurrencySelect
-					onSelect={setCurrency}
-					currencies={["SEK", "USD", "EUR", "COP"]}
-					currency={currency}
-				/>
-			</View>
+    return (
+        <KeyboardAvoidingView
+            style={{
+                ...styles.container,
+                ...styles[backgroundStyle as keyof typeof styles],
+            }}
+            behavior={Platform.OS === "ios" ? "padding" : undefined}
+        >
+            {/* Header */}
+            <View style={styles.header}>
+                <ThemedText type="subtitle">Add Transaction</ThemedText>
+            </View>
 
-			{/* Type */}
-			<Select
-				placeholder={"Transaction type"}
-				items={["Expense", "Income", "Transfer", "Savings"]}
-				onSelect={setType}
-				value={type}
-			/>
+            {/* Amount Section */}
+            <View style={styles.amountContainer}>
+                <Text style={styles.amount}>
+                    {type === "Expense" && "-"}
+                    {amount.length > 0 ? amount : "0.00"}
+                </Text>
+                <CurrencySelect
+                    onSelect={setCurrency}
+                    currencies={["SEK", "USD", "EUR", "COP"]}
+                    currency={currency}
+                />
+            </View>
 
-			{/* Category */}
-			<Select
-				placeholder={"Category"}
-				items={categories?.map((c) => c.name) || []}
-				onSelect={setCategory}
-				value={category}
-			/>
+            {/* Type */}
+            <Select
+                placeholder={"Transaction type"}
+                items={["Expense", "Income", "Transfer", "Savings"]}
+                onSelect={setType}
+                value={type}
+            />
 
-			{/* Account */}
-			<Select
-				placeholder={"Account"}
-				items={accounts?.map((a) => a.name) || []}
-				onSelect={setAccount}
-				value={account}
-			/>
+            {/* Category */}
+            <Select
+                placeholder={"Category"}
+                items={categories?.map((c) => c.name) || []}
+                onSelect={setCategory}
+                value={category}
+            />
 
-			{/* Date */}
-			<DatePicker date={date} onChange={setDate} />
+            {/* Account */}
+            <Select
+                placeholder={"Account"}
+                items={accounts?.map((a) => a.name) || []}
+                onSelect={setAccount}
+                value={account}
+            />
 
-			{/* Description */}
-			<View style={styles.row}>
-				<TextInput
-					style={styles.notesInput}
-					placeholder="Description"
-					value={description}
-					onChangeText={setDescription}
-				/>
-			</View>
+            {/* Date */}
+            <DatePicker date={date} onChange={setDate} />
 
-			{/* Custom Keyboard */}
-			<View style={styles.keyboardContainer}>
-				<View style={styles.keyboard}>
-					{[
-						"1",
-						"2",
-						"3",
-						"4",
-						"5",
-						"6",
-						"7",
-						"8",
-						"9",
-						".",
-						"0",
-						"delete",
-					].map((key) => (
-						<TouchableOpacity
-							key={key}
-							style={styles.key}
-							onPress={() => handleKeyPress(key)}
-						>
-							<Text style={styles.keyText}>{key === "delete" ? "⌫" : key}</Text>
-						</TouchableOpacity>
-					))}
-				</View>
-			</View>
+            {/* Description */}
+            <View style={styles.row}>
+                <TextInput
+                    style={styles.notesInput}
+                    placeholder="Description"
+                    value={description}
+                    onChangeText={setDescription}
+                />
+            </View>
 
-			<TouchableOpacity style={styles.button} onPress={onSubmit}>
-				<ThemedText type="subtitle" style={styles.buttonText}>
-					Save
-				</ThemedText>
-			</TouchableOpacity>
-		</KeyboardAvoidingView>
-	);
+            {/* Custom Keyboard */}
+            <View style={styles.keyboardContainer}>
+                <View style={styles.keyboard}>
+                    {[
+                        "1",
+                        "2",
+                        "3",
+                        "4",
+                        "5",
+                        "6",
+                        "7",
+                        "8",
+                        "9",
+                        ".",
+                        "0",
+                        "delete",
+                    ].map((key) => (
+                        <TouchableOpacity
+                            key={key}
+                            style={styles.key}
+                            onPress={() => handleKeyPress(key)}
+                        >
+                            <Text style={styles.keyText}>{key === "delete" ? "⌫" : key}</Text>
+                        </TouchableOpacity>
+                    ))}
+                </View>
+            </View>
+
+            <TouchableOpacity style={styles.button} onPress={onSubmit}>
+                <ThemedText type="subtitle" style={styles.buttonText}>
+                    Save
+                </ThemedText>
+            </TouchableOpacity>
+        </KeyboardAvoidingView>
+    );
 }
 
 const styles = StyleSheet.create({
-	button: {
-		backgroundColor: "#007AFF",
-		borderRadius: 10,
-		padding: 15,
-		marginTop: 20,
-		alignItems: "center",
-	},
-	buttonText: {
-		color: "#FFF",
-		fontSize: 16,
-		fontWeight: "bold",
-	},
-	container: {
-		flex: 1,
-		padding: 20,
-		backgroundColor: "#F5F5F5",
-	},
-	keyboardContainer: {
-		marginVertical: 20,
-	},
-	header: {
-		marginBottom: 20,
-	},
-	amountContainer: {
-		flexDirection: "row",
-		alignItems: "center",
-		justifyContent: "center",
-		marginVertical: 20,
-	},
-	currency: {
-		fontSize: 20,
-		fontWeight: "600",
-		marginRight: 10,
-	},
-	amount: {
-		fontSize: 36,
-		fontWeight: "bold",
-	},
-	row: {
-		flexDirection: "row",
-		alignItems: "center",
-		justifyContent: "space-between",
-		backgroundColor: "#FFF",
-		borderRadius: 10,
-		padding: 15,
-		marginVertical: 5,
-	},
-	label: {
-		fontSize: 16,
-		fontWeight: "500",
-		color: "#333",
-	},
-	notesInput: {
-		flex: 1,
-		fontSize: 16,
-		fontWeight: "400",
-		color: "#333",
-	},
-	keyboard: {
-		flexDirection: "row",
-		flexWrap: "wrap",
-		justifyContent: "space-around",
-		backgroundColor: "white",
-	},
-	key: {
-		width: "33.33%",
-		padding: 15,
-		alignItems: "center",
-		justifyContent: "center",
-		borderWidth: StyleSheet.hairlineWidth,
-		borderColor: "#ccc",
-	},
-	keyText: {
-		fontSize: 24,
-		fontWeight: "bold",
-	},
-	expenseBg: {
-		backgroundColor: "#e74c3c",
-	},
-	incomeBg: {
-		backgroundColor: "#2ecc71",
-	},
-	transactionBg: {
-		backgroundColor: "#3498db",
-	},
-	savingsBg: {
-		backgroundColor: "#f1c40f",
-	},
+    button: {
+        backgroundColor: "#007AFF",
+        borderRadius: 10,
+        padding: 15,
+        marginTop: 20,
+        alignItems: "center",
+    },
+    buttonText: {
+        color: "#FFF",
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: "#F5F5F5",
+    },
+    keyboardContainer: {
+        marginVertical: 20,
+    },
+    header: {
+        marginBottom: 10,
+    },
+    amountContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        marginVertical: 10,
+    },
+    currency: {
+        fontSize: 20,
+        fontWeight: "600",
+        marginRight: 10,
+    },
+    amount: {
+        fontSize: 36,
+        fontWeight: "bold",
+    },
+    row: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        backgroundColor: "#FFF",
+        borderRadius: 10,
+        padding: 15,
+        marginVertical: 5,
+    },
+    label: {
+        fontSize: 16,
+        fontWeight: "500",
+        color: "#333",
+    },
+    notesInput: {
+        flex: 1,
+        fontSize: 16,
+        fontWeight: "400",
+        color: "#333",
+    },
+    keyboard: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "space-around",
+        backgroundColor: "white",
+    },
+    key: {
+        width: "33.33%",
+        padding: 15,
+        alignItems: "center",
+        justifyContent: "center",
+        borderWidth: StyleSheet.hairlineWidth,
+        borderColor: "#ccc",
+    },
+    keyText: {
+        fontSize: 24,
+        fontWeight: "bold",
+    },
+    expenseBg: {
+        backgroundColor: "#e74c3c",
+    },
+    incomeBg: {
+        backgroundColor: "#2ecc71",
+    },
+    transactionBg: {
+        backgroundColor: "#3498db",
+    },
+    savingsBg: {
+        backgroundColor: "#f1c40f",
+    },
 });
