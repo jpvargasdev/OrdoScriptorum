3ff34e77a0f9962f62a036a4ffca6abfac23b51f
Juan Vargas <vargasm.jp@gmail.com>
2024-12-25 19:24:06 +0100

    fix: percentage calculation

--- components/ui/BudgetsGraph.tsx
+++ components/ui/BudgetsGraph.tsx
@@ -1,53 +1,50 @@
-import { View, StyleSheet, Dimensions } from "react-native";
-import { ThemedView } from "../ThemedView";
-import { BarChart, PieChart } from "react-native-gifted-charts";
+import { View, StyleSheet } from "react-native";
+import { PieChart } from "react-native-gifted-charts";
 import { ThemedText } from "../ThemedText";
 import React from "react";
 
-const { width } = Dimensions.get("window");
-
 function getChartData(budget: BudgetSummary) {
-	const { needs_amount, needs_budget, needs_percentage } = budget;
-	const { wants_amount, wants_budget } = budget;
-	const { savings_amount, savings_budget } = budget;
+	const { needs_percentage } = budget;
+	const { wants_percentage } = budget;
+	const { savings_percentage } = budget;
 
 	const pieData = {
 		needs: [
 			// Needs
 			{
-				value: (needs_amount / needs_budget) * 100,
-				text: `${((needs_amount / needs_budget) * 100).toFixed(1)}% Spent`,
+				value: needs_percentage,
+				text: `${needs_percentage}% Spent`,
 				color: "#79D2DE", // Light Blue for Needs Spent
 			},
 			{
-				value: 100 - (needs_amount / needs_budget) * 100,
-				text: `${(100 - (needs_amount / needs_budget) * 100).toFixed(1)}% Remaining`,
+				value: 100 - needs_percentage,
+				text: `${(100 - needs_percentage).toFixed(1)}% Remaining`,
 				color: "#FFABAB", // Light Red for Needs Remaining
 			},
 		],
 		wants: [
 			// Wants
 			{
-				value: (wants_amount / wants_budget) * 100,
-				text: `${((wants_amount / wants_budget) * 100).toFixed(1)}% Spent`,
+				value: wants_percentage,
+				text: `${wants_percentage}% Spent`,
 				color: "#ED6665", // Bright Red for Wants Spent
 			},
 			{
-				value: 100 - (wants_amount / wants_budget) * 100,
-				text: `${(100 - (wants_amount / wants_budget) * 100).toFixed(1)}% Remaining`,
+				value: 100 - wants_percentage,
+				text: `${(100 - wants_percentage).toFixed(1)}% Remaining`,
 				color: "#FFC107", // Yellow for Wants Remaining
 			},
 		],
 		savings: [
 			// Savings
 			{
-				value: (savings_amount / savings_budget) * 100,
-				text: `${((savings_amount / savings_budget) * 100).toFixed(1)}% Spent`,
+				value: savings_percentage,
+				text: `${savings_percentage}% Spent`,
 				color: "#4CAF50", // Green for Savings Spent
 			},
 			{
-				value: 100 - (savings_amount / savings_budget) * 100,
-				text: `${(100 - (savings_amount / savings_budget) * 100).toFixed(1)}% Remaining`,
+				value: 100 - savings_percentage,
+				text: `${(100 - savings_percentage).toFixed(1)}% Remaining`,
 				color: "#8BC34A", // Light Green for Savings Remaining
 			},
 		],
@@ -75,7 +72,7 @@ export function BudgetsGraph({ budget }: { budget: BudgetSummary | null }) {
 						</ThemedText>
 						<ThemedText type="default">-{budget.wants_amount} SEK</ThemedText>
 					</View>
-					<PieChart radius={40} data={data.wants} />
+					<PieChart radius={40} data={data.wants} showText />
 				</View>
 			</View>
 			<View style={styles.barContainer}>
@@ -89,7 +86,7 @@ export function BudgetsGraph({ budget }: { budget: BudgetSummary | null }) {
 						</ThemedText>
 						<ThemedText type="default">-{budget.needs_amount} SEK</ThemedText>
 					</View>
-					<PieChart radius={40} data={data.needs} />
+					<PieChart radius={40} data={data.needs} showText />
 				</View>
 			</View>
 			<View style={styles.barContainer}>
@@ -103,7 +100,7 @@ export function BudgetsGraph({ budget }: { budget: BudgetSummary | null }) {
 						</ThemedText>
 						<ThemedText type="default">-{budget.savings_amount} SEK</ThemedText>
 					</View>
-					<PieChart radius={40} data={data.savings} />
+					<PieChart radius={40} data={data.savings} showText />
 				</View>
 			</View>
 		</View>
