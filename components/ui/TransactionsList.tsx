bdbfd29210e19cbff33ac4172aa87f9d78f1d2db
Juan Vargas <53348816+jpvargasdev@users.noreply.github.com>
2025-12-23 10:03:12 +0100

    chore: fix warnings, update icons (#5)

--- components/ui/TransactionsList.tsx
+++ components/ui/TransactionsList.tsx
@@ -8,12 +8,14 @@ export default function TransactionsList({
 	loading,
 	onDeleteTransaction,
 	onRefresh,
+	ListHeaderComponent, // <-- Nueva prop
 }: {
 	transactions: Transaction[] | null;
 	accounts: Account[] | null;
 	loading: boolean;
 	onDeleteTransaction?: (transaction: Transaction) => void;
 	onRefresh?: () => void;
+	ListHeaderComponent?: React.ReactElement; // <-- Tipo para el header
 }) {
 	return (
 		<FlatList
@@ -27,11 +29,15 @@ export default function TransactionsList({
 				/>
 			)}
 			style={styles.transactions}
+			// Esto permite que el Header haga scroll con la lista
+			ListHeaderComponent={ListHeaderComponent}
 			ListEmptyComponent={
 				<ThemedText type="default">No transactions</ThemedText>
 			}
 			refreshing={loading}
 			onRefresh={onRefresh}
+			// Agregamos esto para que el scroll se sienta bien al final
+			contentContainerStyle={styles.contentContainer}
 		/>
 	);
 }
@@ -39,5 +45,8 @@ export default function TransactionsList({
 const styles = StyleSheet.create({
 	transactions: {
 		flex: 1,
+	},
+	contentContainer: {
+		paddingBottom: 20,
 	},
 });
